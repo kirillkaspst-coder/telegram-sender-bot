6d43ddf14ea79556a92927fee19f95fe9113ab0e
kirillkaspst-coder <kirillkaspst@gmail.com>
2026-03-11 18:27:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import asyncio
 import random
 import os
 
-api_id = int(os.getenv("32341939"))
-api_hash = os.getenv("6c4cb3ee1bfa7c2640cfff2026b27ceb")
+api_id = 32341939
+api_hash = "6c4cb3ee1bfa7c2640cfff2026b27ceb"
 
 groups = [
-    "https://t.me/provrkahsjdjx",
+    "t.me/YesBroChat",
 ]
 
-message = "🟥КyПЛy aЛЬФA Б@НК ЛИЧНЫЙ К@БИНЕТ 🟩
-14+ 8.500 
+message = '''
+🟥КyПЛy aЛЬФA Б@НК ЛИЧНЫЙ К@БИНЕТ 🟩
+14+ 8.500
 16+ 11.000
 18+ 14.000
 Скyп@ю лк Тинькофф писать в лс
-@Sherek_2"
+@Sherek_2
+'''
 
 client = TelegramClient("session", api_id, api_hash)
 
@@ -25,7 +27,7 @@ async def main():
             try:
                 await client.send_message(group, message)
                 print("Отправлено:", group)
-                await asyncio.sleep(random.randint(60,180))
+                await asyncio.sleep(random.randint(180,240))
             except Exception as e:
                 print("Ошибка:", e)
 
